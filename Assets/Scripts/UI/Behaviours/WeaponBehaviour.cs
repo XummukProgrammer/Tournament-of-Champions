1e8d995a3efe0b53a241bd6310f775a9f5d2d34e
Andrey Grebenshchikov <a-grebenshikov@bk.ru>
2022-12-12 00:44:24 +0500

    Добавлено изображение пистолета

--- Assets/Scripts/UI/Behaviours/WeaponBehaviour.cs
+++ Assets/Scripts/UI/Behaviours/WeaponBehaviour.cs
@@ -2,13 +2,10 @@ using UnityEngine;
 
 public class WeaponBehaviour : MonoBehaviour
 {
-    [SerializeField] private TMPro.TMP_Text _name;
     [SerializeField] private TMPro.TMP_Text _ammo;
 
     public void Init(Weapon weapon)
     {
-        _name.text = weapon.Id;
-
         weapon.AmmoChanged += OnWeaponAmmoChanged;
 
         OnWeaponAmmoChanged(weapon.Ammo);
